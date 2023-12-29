8bb1b4d5bf64d58d4a4c1c6eb7c2222af8d440fb
Kazuya Takahashi <electrolysis.jp@gmail.com>
2023-12-30 01:09:04 +0900

    Implement card.CardBase

--- src/binguistics/card.py
+++ src/binguistics/card.py
@@ -42,3 +42,80 @@ class LineMask:
             )
 
         return cls._instances[m]
+
+
+class CardBase:
+    """
+    This is the base class that represents a bingo card.
+
+    A card has four important attributes: size, state, square ID, and free squares.
+    If a card has a size of `n`, that means the card has `n*n` squares.
+    The state of a card shows which squares are filled and which are not.
+    It is an `n*n`-bit integer and its set bits correspond to filled squares.
+    The `k`-th bit of a state integer means the square with the ID of `k`.
+    For example, on a bingo card of size 5, the IDs are arranged as follows.
+
+         0  1  2  3  4
+         5  6  7  8  9
+        10 11 12 13 14
+        15 16 17 18 19
+        20 21 22 23 24
+    """
+
+    from collections.abc import Iterable
+
+    def __init__(self, size: int, state: int = 0, free: Iterable[int] = ()):
+        """
+        Parameters
+        ----------
+        size : int
+            Card's size
+        state : int, optional
+            Initial state, by default 0
+        free : Iterable[int], optional
+            IDs of free squares, by default ()
+        """
+
+        self._size = size
+        coverall_mask = (1 << size**2) - 1
+        self._state = state & coverall_mask
+        self._free = tuple(sorted(set(i for i in free if 0 <= i < size**2)))
+        for i in self._free:
+            self._state |= 1 << i
+
+    @property
+    def size(self) -> int:
+        """
+        The card size.
+
+        Returns
+        -------
+        int
+            The card size.
+        """
+        return self._size
+
+    @property
+    def state(self) -> int:
+        """
+        The internal card state.
+
+        Returns
+        -------
+        int
+            The card's current state, indicating which squares are filled and which are not.
+        """
+        return self._state
+
+    @property
+    def free(self) -> tuple[int]:
+        """
+        Free squares in the card.
+
+        Returns
+        -------
+        tuple[int]
+            IDs of the free squares in the card.
+        """
+
+        return self._free
--- tests/test_card.py
+++ tests/test_card.py
@@ -18,3 +18,80 @@ def test_LineMask_1():
 
     assert mask3.DIAGONAL_1 == 0b100_010_001
     assert mask3.DIAGONAL_2 == 0b001_010_100
+
+
+# graceful initializations & getters
+
+
+def test_init_1():
+    c = card.CardBase(3)
+    assert c._size == 3
+    assert c.size == 3
+    assert c._state == 0
+    assert c.state == 0
+    assert c.free == ()
+    assert c.free == ()
+
+
+def test_init_2():
+    s = 0b1110_0101_1100_0000
+    c = card.CardBase(4, s)
+    assert c._size == 4
+    assert c.size == 4
+    assert c._state == s
+    assert c.state == s
+    assert c._free == ()
+    assert c.free == ()
+
+
+def test_init_3():
+    s = 0b1110_0101_1100_0000
+    f = (5, 2, 4)
+    s_f = 0b1110_0101_1111_0100
+    c = card.CardBase(4, s, f)
+    assert c._state == s_f
+    assert c.state == s_f
+    assert c._free == (2, 4, 5)
+    assert c.free == (2, 4, 5)
+
+
+# evil initializations & getters
+
+
+def test_init_101():
+    bad_f = (1, 1, 1, 2)
+    s_f = 0b0000_0000_0000_0110
+    c = card.CardBase(4, free=bad_f)
+    assert c._state == s_f
+    assert c.state == s_f
+    assert c._free == (1, 2)
+    assert c.free == (1, 2)
+
+
+def test_init_102():
+    bad_f = (-1, 1, -111, 200, 200)
+    s_f = 0b0000_0000_0000_0010
+    c = card.CardBase(4, free=bad_f)
+    assert c._state == s_f
+    assert c.state == s_f
+    assert c._free == (1,)
+    assert c.free == (1,)
+
+
+def test_init_103():
+    bad_s = 0b11_000_010_000_010
+    good_s = 0b010_000_010
+    c = card.CardBase(3, bad_s)
+    assert c._state == good_s
+    assert c.state == good_s
+
+
+def test_init_104():
+    bad_f = (1, 0, 1, -1, -12, 999, 4)
+    bad_s = 0b11_000_010_000_010
+    good_s = 0b010_010_011
+    c = card.CardBase(3, bad_s, bad_f)
+    assert c._state == good_s
+    assert c.state == good_s
+    assert c._free == (0, 1, 4)
+    assert c.free == (0, 1, 4)
